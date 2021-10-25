cd79dd584c9fc7197f63a594cb1c6b77bea67dd6
Mahim Anjum Uchchhwas <mauchchhash@gmail.com>
2021-10-26 00:15:27 +0600

    get services list based on hair_size & hair_type

--- app/Repositories/ServicesRepository.php
+++ app/Repositories/ServicesRepository.php
@@ -14,4 +14,48 @@ class ServicesRepository extends BaseRepository
     {
         $this->model = $model;
     }
+
+    /**
+     * get all the items collection from database table using model.
+     *
+     * @return Collection of items.
+     */
+    public function get(Request $request)
+    {
+        $model = $this->model;
+
+        if($request->has('hair_size')) {
+            $model->where('hair_size', $request->hair_size);
+        }
+
+        if($request->has('hair_type')) {
+            $model->where('hair_type', $request->hair_type);
+        }
+
+        return $model
+            ->orderBy($request->input('orderBy', 'created_at'), $request->input('sort', 'desc'))
+            ->get();
+    }
+
+    /**
+     * get collection of items in paginate format.
+     *
+     * @return Collection of items.
+     */
+    public function paginate(Request $request)
+    {
+        $model = $this->model;
+
+        if($request->has('hair_size')) {
+            $model->where('hair_size', $request->hair_size);
+        }
+
+        if($request->has('hair_type')) {
+            $model->where('hair_type', $request->hair_type);
+        }
+
+        return $model
+            ->orderBy($request->input('orderBy', 'created_at'), $request->input('sort', 'desc'))
+            ->paginate($request->input('limit', 10));
+    }
 }
